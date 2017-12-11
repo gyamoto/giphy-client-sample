587de9b7b0a9d7584411b492978b23fa7487cb95
山本晃平 <yamamotokohei@team-lab.com>
2017-12-11 19:28:04 +0900

    各画面へ遷移するレイアウトの実装 AutosizingTextView https://developer.android.com/guide/topics/ui/look-and-feel/autosizing-textview.html 実装

--- app/src/main/java/com/kyamamoto/kodein/ui/MainActivity.kt
+++ app/src/main/java/com/kyamamoto/kodein/ui/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.kyamamoto.kodein.ui
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.kyamamoto.kodein.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trend.setOnClickListener {
+            Snackbar.make(container, R.string.trend, Snackbar.LENGTH_SHORT).show()
+        }
+
+        random.setOnClickListener {
+            Snackbar.make(container, R.string.random, Snackbar.LENGTH_SHORT).show()
+        }
+
+        search.setOnClickListener {
+            Snackbar.make(container, R.string.search, Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
