3eaaac2427f88e10e71852ef269587f752cbdecb
山本晃平 <yamamotokohei@team-lab.com>
2017-12-15 20:04:03 +0900

    Timberの設定コミット漏れ

--- app/src/main/java/com/kyamamoto/kodein/KodeinApplication.kt
+++ app/src/main/java/com/kyamamoto/kodein/KodeinApplication.kt
@@ -1,8 +1,20 @@
 package com.kyamamoto.kodein
 
 import android.app.Application
+import timber.log.Timber
 
 /**
  * Created by kyamamoto on 2017/11/07.
  */
-class KodeinApplication : Application()
+class KodeinApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
