d334924bd569e4b46e5ba29bb3c2316347f778d9
山本晃平 <yamamotokohei@team-lab.com>
2018-02-22 21:02:20 +0900

    ランダムのLiveDataに永続化予定のストアを追加

--- app/src/main/kotlin/com/kyamamoto/kodein/ui/random/RandomLiveData.kt
+++ app/src/main/kotlin/com/kyamamoto/kodein/ui/random/RandomLiveData.kt
@@ -1,21 +1,27 @@
 package com.kyamamoto.kodein.ui.random
 
 import android.arch.lifecycle.LiveData
-import com.kyamamoto.giphy.ApiBuilder
+import com.kyamamoto.giphy.Api
 import com.kyamamoto.giphy.model.RandomResponse
 import com.kyamamoto.kodein.architecturecomponent.Resource
+import com.kyamamoto.persistentrandom.Store
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by kyamamoto on 2018/02/05.
  */
-class RandomLiveData : LiveData<Resource<RandomResponse>>() {
-
-    private val api = ApiBuilder.build()
+class RandomLiveData(
+        private val api: Api,
+        private val store: Store<RandomResponse>
+) : LiveData<Resource<RandomResponse>>() {
 
     init {
-        request()
+        store.item?.let {
+            value = Resource.success(it)
+        } ?: run {
+            request()
+        }
     }
 
     fun request() {
@@ -26,6 +32,7 @@ class RandomLiveData : LiveData<Resource<RandomResponse>>() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     value = Resource.success(it)
+                    store.item = it
                 }, {
                     value = Resource.error(value?.response, it)
                 })
--- app/src/main/kotlin/com/kyamamoto/kodein/ui/random/RandomViewModel.kt
+++ app/src/main/kotlin/com/kyamamoto/kodein/ui/random/RandomViewModel.kt
@@ -3,15 +3,19 @@ package com.kyamamoto.kodein.ui.random
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
+import com.kyamamoto.giphy.ApiBuilder
 import com.kyamamoto.giphy.model.RandomResponse
 import com.kyamamoto.kodein.architecturecomponent.Resource
+import com.kyamamoto.persistentrandom.RamdomResponseStore
 
 /**
  * Created by kyamamoto on 2018/02/05.
  */
 class RandomViewModel : ViewModel() {
 
-    private val liveData: RandomLiveData by lazy { RandomLiveData() }
+    private val liveData: RandomLiveData by lazy {
+        RandomLiveData(ApiBuilder.build(), RamdomResponseStore())
+    }
 
     val random: LiveData<Resource<RandomResponse>> = Transformations.map(liveData, { it })
 
